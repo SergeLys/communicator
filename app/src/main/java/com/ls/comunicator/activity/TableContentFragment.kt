f3540ac43345ef668da714bafb8a2e0f83734958
SergeLys <lisekno.1996@mail.ru>
2020-04-12 22:10:38 +0300

    table content updating

--- app/src/main/java/com/ls/comunicator/activity/TableContentFragment.kt
+++ app/src/main/java/com/ls/comunicator/activity/TableContentFragment.kt
@@ -10,45 +10,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.textfield.TextInputEditText
 import com.ls.comunicator.R
 import com.ls.comunicator.adapter.ContentTableAdapter
 import com.ls.comunicator.core.SingletonCard
+import kotlinx.android.synthetic.main.fragment_table_content.view.*
 
 
 class TableContentFragment(val tabLayout: TabLayout) : Fragment() {
-
-    lateinit var searchEditText: TextInputEditText
-    lateinit var contentList: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_table_content, container, false)
-        searchEditText = view.findViewById(R.id.search_page_text)
-        contentList = view.findViewById(R.id.content_table_list)
         val layoutManager = LinearLayoutManager( context, RecyclerView.VERTICAL, false)
-        contentList.layoutManager = layoutManager
-        contentList.adapter = ContentTableAdapter(SingletonCard.pages, tabLayout, context)
-
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (p0.toString() != "") {
-                    SingletonCard.pages.forEachIndexed { index, s ->
-                        if (s.contains(p0.toString())) {
-                            layoutManager.scrollToPositionWithOffset(index, 0)
-                        }
-                    }
-                }
-            }
-        })
+        view.contentTableList.layoutManager = layoutManager
+        view.contentTableList.adapter = ContentTableAdapter(SingletonCard.pages, tabLayout, context)
         return view
     }
 
